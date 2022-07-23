bdc6452bb36912cc77935c180e8220cbe3163b97
Sina Kian <70090343+kiansina@users.noreply.github.com>
2022-07-23 16:40:36 +0200

    Update str.py

--- str.py
+++ str.py
@@ -1,17 +1,17 @@
 import streamlit as st
 import pandas as pd
-import psycopg2
-import hidden
+#import psycopg2
+#import hidden
 
-secrets = hidden.secrets()
+#secrets = hidden.secrets()
 
 
-conn = psycopg2.connect(host=secrets['host'],
-        port=secrets['port'],
-        database=secrets['database'],
-        user=secrets['user'],
-        password=secrets['pass'],
-        connect_timeout=3)
+#conn = psycopg2.connect(host=secrets['host'],
+#        port=secrets['port'],
+#        database=secrets['database'],
+#        user=secrets['user'],
+#        password=secrets['pass'],
+#        connect_timeout=3)
 
 
 @st.cache(allow_output_mutation=True)
